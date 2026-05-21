dedff4685810d8b73f269fb2a6b13be4d6176666
Capstoner1 <cxg70028090@gmail.com>
2026-05-21 00:45:28 -0400

    Add files via upload

---
+++ version.h
@@ -0,0 +1,9 @@
+#pragma once
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 0
+#define VERSION_PATCH 0
+#define VERSION_BUILD 44
+
+#define stringify(a) # a
+#define expand_and_stringify(a) stringify(a)
+#define plugin_version expand_and_stringify(VERSION_MAJOR) "." expand_and_stringify(VERSION_MINOR) "." expand_and_stringify(VERSION_PATCH)
